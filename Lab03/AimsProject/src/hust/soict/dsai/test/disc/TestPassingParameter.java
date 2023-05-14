27a4ee6058f6435af320d786bec4aee681a9f286
BinhNguyenBG <130740759+BinhNguyenBG@users.noreply.github.com>
2023-05-14 22:03:51 +0700

    Add files via upload

--- Lab03/AimsProject/src/hust/soict/dsai/test/disc/TestPassingParameter.java
+++ Lab03/AimsProject/src/hust/soict/dsai/test/disc/TestPassingParameter.java
@@ -16,10 +16,10 @@ public class TestPassingParameter {
 		System.out.println("jungle dvd title: " + jungleDVD.getTitle());
 	}
 	
-	public static void swap(Object o1, Object o2) {
-		Object tmp = o1;
-		o1 = o2;
-		o2 = tmp;
+	public static void swap(DigitalVideoDisc o1, DigitalVideoDisc o2) {
+		String o1OldTitle = o1.getTitle();
+		o1.setTitle(o2.getTitle());
+		o2.setTitle(o1OldTitle);
 	}
 	
 	public static void changeTitle(DigitalVideoDisc dvd, String title) {
