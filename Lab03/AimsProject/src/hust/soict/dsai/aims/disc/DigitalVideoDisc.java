cc931aa44f8a652aa1c54cff6754a6d1bc570485
BinhNguyenBG <130740759+BinhNguyenBG@users.noreply.github.com>
2023-05-15 20:01:02 +0700

    Add files via upload

--- Lab03/AimsProject/src/hust/soict/dsai/aims/disc/DigitalVideoDisc.java
+++ Lab03/AimsProject/src/hust/soict/dsai/aims/disc/DigitalVideoDisc.java
@@ -1,6 +1,4 @@
 package hust.soict.dsai.aims.disc;
-
-
 public class DigitalVideoDisc {
 	
 	private String title;
@@ -10,6 +8,22 @@ public class DigitalVideoDisc {
 	private float cost;
 	private int id;
 	private static int nbDigitalVideoDiscs = 0;
+	
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public void setDirector(String director) {
+		this.director = director;
+	}
+	public void setLength(int length) {
+		this.length = length;
+	}
+	public void setCost(float cost) {
+		this.cost = cost;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
 	
 	public String getTitle() {
 		return title;
